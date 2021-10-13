0c051248ec873e754076f63116b99f4d8392f7d5
諏訪守 <suwamamoru@suwamamorunoMacBook-Air.local>
2021-10-13 16:32:34 +0900

    修正完了

--- src/main.js
+++ src/main.js
@@ -3,16 +3,10 @@ const taskTrigger = document.getElementById('task-trigger');
 const taskChange = document.getElementById('task-change');
 const taskValue = document.getElementById('task-value');
 const taskTable = document.getElementById('task-table');
-const workTable = document.getElementById('work-table');
-const finishTable = document.getElementById('finish-table');
 const tasks = [];
-const works = [];
-const finishes = [];
 
 const showTasks = () => {
   taskTable.innerText = '';
-  workTable.innerText = '';
-  finishTable.innerText = '';
   tasks.forEach((task) => {
     const taskId = taskTable.rows.length;
     const row = taskTable.insertRow(-1);
@@ -24,96 +18,26 @@ const showTasks = () => {
 
     id.innerText = taskId;
     comment.innerText = task.task;
-    createStatusButton(task, status);
+    createStatusButton(task, status, row);
     createRemoveButton(task, remove, row);
   });
 }
 
-const workTasks = () => {
-  taskTable.innerText = '';
-  workTable.innerText = '';
-  finishTable.innerText = '';
-  works.forEach((work) => {
-    const workId = workTable.rows.length;
-    const row = workTable.insertRow(-1);
-
-    const id = row.insertCell(0);
-    const comment = row.insertCell(1);
-    const status = row.insertCell(2);
-    const remove = row.insertCell(3);
-
-    id.innerText = workId;
-    comment.innerText = work.task;
-    createStatusButton(work, status);
-    createRemoveButton(work, remove, row);
-  });
-}
-
-const finishTasks = () => {
-  taskTable.innerText = '';
-  workTable.innerText = '';
-  finishTable.innerText = '';
-  finishes.forEach((finish) => {
-    const finishId = finishTable.rows.length;
-    const row = finishTable.insertRow(-1);
-
-    const id = row.insertCell(0);
-    const comment = row.insertCell(1);
-    const status = row.insertCell(2);
-    const remove = row.insertCell(3);
-
-    id.innerText = finishId;
-    comment.innerText = finish.task;
-    createStatusButton(finish, status);
-    createRemoveButton(finish, remove, row);
-  });
-}
-
-const ifWork = () => {
-  works.length = 0;
-  const beforeWork = tasks.filter((task) => {
-    return task.status === '作業中';
-  });
-  for (let i = 0; i < beforeWork.length; i++) {
-    const work = beforeWork[i];
-    works.push(work);
-  }
-}
-
-const ifFinish = () => {
-  finishes.length = 0;
-  const beforeFinish = tasks.filter((task) => {
-    return task.status === '完了';
-  });
-  for (let i = 0; i < beforeFinish.length; i++) {
-    const finish = beforeFinish[i];
-    finishes.push(finish);
-  }
-}
-
-const ifChange = () => {
-  if (taskChange.group.value === 'working') {
-    ifWork();
-    workTasks();
-  } else if (taskChange.group.value === 'finish') {
-    ifFinish();
-    finishTasks();
-  } else {
-    showTasks();
-  }
-}
-
-const createStatusButton = (task, status) => {
+const createStatusButton = (task, status, row) => {
   const statusButton = document.createElement('button');
   statusButton.innerText = task.status;
   statusButton.addEventListener('click', () => {
+    const index = row.rowIndex - 1;
     if (task.status === '作業中') {
       task.status = '完了';
-      ifChange();
+      taskTable.rows[index].className = 'finish';
     } else {
       task.status = '作業中';
-      ifChange();
+      taskTable.rows[index].className = 'working';
     }
+    statusButton.remove();
+    createStatusButton(task, status, row);
+    displayChange();
   });
   status.appendChild(statusButton);
 }
@@ -123,43 +47,57 @@ const createRemoveButton = (task, remove, row) => {
   removeButton.innerText = task.remove;
   removeButton.addEventListener('click', () => {
     const index = row.rowIndex - 1;
-    if (taskChange.group.value === 'working') {
-      ifWork();
-      const workSplice = works.splice(index,1);
-      const foundWork = workSplice.find((work) => work.status === '作業中');
-      const taskSplice = tasks.filter((task) => task.task !== foundWork.task);
-      tasks.length = 0;
-      for (let i = 0; i < taskSplice.length; i++) {
-        const taskReunion = taskSplice[i];
-        tasks.push(taskReunion);
-      }
-      workTasks();
-    } else if (taskChange.group.value === 'finish') {
-      ifFinish();
-      const finishSplice = finishes.splice(index,1);
-      const foundFinish = finishSplice.find((finish) => finish.status === '完了');
-      const taskSplice = tasks.filter((task) => task.task !== foundFinish.task);
-      tasks.length = 0;
-      for (let i = 0; i < taskSplice.length; i++) {
-        const taskReunion = taskSplice[i];
-        tasks.push(taskReunion);
-      }
-      finishTasks();
-    } else {
-      tasks.splice(index,1);
-      showTasks();
-    }
+    tasks.splice(index,1);
+    showTasks();
+    displayChange();
   });
   remove.appendChild(removeButton);
 }
 
 taskTrigger.addEventListener('click', () => {
-  const task = {task: taskValue.value, status: '作業中', remove: '削除'};
-  tasks.push(task);
-  ifChange();
-  taskValue.value = '';
+  if (taskValue.value !== '') {
+    const task = {task: taskValue.value, status: '作業中', remove: '削除'};
+    tasks.push(task);
+    showTasks();
+    displayChange();
+    taskValue.value = '';
+  }
 });
 
+const displayChange = () => {
+  for (let i = 0; i < taskTable.rows.length; i++) {
+    if (taskTable.rows[i].cells[2].innerText === '作業中') {
+      taskTable.rows[i].className = 'working';
+    } else if (taskTable.rows[i].cells[2].innerText === '完了') {
+      taskTable.rows[i].className = 'finish';
+    }
+  }
+  const workTask = document.querySelectorAll('.working');
+  const finishTask = document.querySelectorAll('.finish');
+  if (taskChange.group.value === 'working') {
+    workTask.forEach(element => {
+      element.style.display = '';
+    });
+    finishTask.forEach(element => {
+      element.style.display = 'none';
+    });
+  } else if (taskChange.group.value === 'finish') {
+    workTask.forEach(element => {
+      element.style.display = 'none';
+    });
+    finishTask.forEach(element => {
+      element.style.display = '';
+    });
+  } else {
+    workTask.forEach(element => {
+      element.style.display = '';
+    });
+    finishTask.forEach(element => {
+      element.style.display = '';
+    });
+  }
+};
+
 taskChange.addEventListener('change', () => {
-  ifChange();
+  displayChange();
 });
